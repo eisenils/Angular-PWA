858192eb014711a60a06431b5d03476e093189c9
eisenils <mail.eisenils@gmail.com>
2021-01-28 11:12:16 +0100

    Lab #9

---
+++ src/app/todo.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import Dexie from 'dexie';
+import { Todo } from './todo';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService extends Dexie {
+  todos: Dexie.Table<Todo, string>;
+
+  constructor() {
+    super('TodoDatabase');
+    this.version(1).stores({
+      todos: 'id',
+    });
+  }
+
+  add(title: string): Promise<any> {
+    const id = uuidv4();
+    return this.todos.add({ id, title, done: false });
+  }
+
+  getAll(): Promise<Todo[]> {
+    return this.todos.toArray();
+  }
+}
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,26 @@
+import { TodoService } from './../todo.service';
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
-
-  constructor() { }
+  todos: Todo[] = [];
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.getAll();
   }
 
+  async getAll() {
+    this.todos = await this.todoService.getAll();
+  }
+
+  async add(title: string) {
+    await this.todoService.add(title);
+    this.getAll();
+  }
 }
